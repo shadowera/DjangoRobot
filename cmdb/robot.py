46d44a72a782679d6cc7ccf9ea5b5bdcc90137e1
guishuai <gui.shuai@kuyun.com>
2017-11-27 16:06:01 +0800

    commit 2

--- cmdb/robot.py
+++ cmdb/robot.py
@@ -1,4 +1,5 @@
 # Filename: robot.py
+import json
 
 from werobot import WeRoBot
 
@@ -7,7 +8,7 @@ robot.config['APP_ID'] = 'wx860aedabf993a256'
 robot.config['APP_SECRET'] = '05e69d96a6953f2cd4840fc1a1c073be'
 
 
-@robot.handler
+@robot.text
 def hello(message):
     return 'Hello World!'
 
@@ -15,3 +16,14 @@ def hello(message):
 @robot.error_page
 def make_error_page(url):
     return "<h1>喵喵喵 %s 不是给麻瓜访问的快走开</h1>" % url
+
+
+@robot.scan
+def scan_code(message):
+    return "mac 是 %s" % message.key
+
+
+@robot.subscribe
+def on_subscribe(message):
+    info = json.dumps(robot.client.get_user_info(message.source))
+    return 'hello %s' % info['nickname']
