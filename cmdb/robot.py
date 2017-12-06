e048b0ac5766db46f0b22e9a07d534c9b7bcf83c
guishuai <gui.shuai@kuyun.com>
2017-12-06 18:01:17 +0800

    add baidu voice

--- cmdb/robot.py
+++ cmdb/robot.py
@@ -1,14 +1,28 @@
 # Filename: robot.py
 import json
+from aip import AipSpeech
 from cmdb.logger import logger
 import time
-from cmdb.media_manager import recognize_voice
 import requests
 from werobot import WeRoBot
 
 robot = WeRoBot(token='kuyunhudong')
 robot.config['APP_ID'] = 'wxb067c8b8e210e780'
 robot.config['APP_SECRET'] = '057c03777f4fac33b71ec23f1d85a1c7'
+
+APP_ID = '10485551'
+API_KEY = 'AKZcOwQvhemGLxre8INXtGas'
+SECRET_KEY = 'cc1ead9bbcb8278ed6b106fe3fecce4c'
+aipSpeech = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
+
+
+def recognize_voice(media_id):
+    url = 'http://file.api.weixin.qq.com/cgi-bin/media/get?access_token=%s&media_id=%s' % (
+        robot.client.get_access_token(), media_id)
+    aipSpeech.asr('', 'pcm', 16000, {
+        'url': url,
+        'callback': 'http://xxx.com/receive',
+    })
 
 
 @robot.text
